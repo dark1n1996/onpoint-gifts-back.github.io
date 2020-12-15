a1670c10cd92bc87e87da2b2931f26a052730298
Vladislav Geyer <vladislav.geyer@yandex.ru>
2020-12-15 19:27:58 +0300

    fix user

--- models/user.js
+++ models/user.js
@@ -32,6 +32,9 @@ const userSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  isAdmin: {
+    type: Boolean
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
